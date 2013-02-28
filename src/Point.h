9633e34533279c26ac1dab7297120ba79e3a5266
Ruben Van Assche <rubenvanassche@gmail.com>
2013-02-28 16:20:02 +0100

    header of point class

--- src/Point.h
+++ src/Point.h
@@ -10,8 +10,29 @@
 #ifndef POINT_H_
 #define POINT_H_
 
-class Point {
+#include <iostream>
+#include "DesignByContract.h"
 
+class Point {
+friend std::ostream& operator<<(std::ostream &stream, Point &p);
+public:
+	Point();
+	Point(int X, int Y);
+	void set(int X, int Yco);
+	void setX(int X);
+	void setY(int Y);
+	int X();
+	int Y();
+
+	Point operator+(Point &p);
+	Point operator-(Point &p);
+	int operator<(Point &p);
+  	int operator>(Point &p);
+  	bool operator==(Point &p);
+  	bool operator!=(Point &p);
+private:
+	int Xcoordinate;
+	int Ycoordinate;
 };
 
 
