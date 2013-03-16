9e541e5269b98fca55a3db6648670ef9780815b8
Stijn-Flipper <w.stijn@gmail.com>
2013-03-16 10:02:02 +0100

    check the 0_SIMULATOR, this his our final version

--- CITY_PROPER_VERSION/0_SIMULATOR.cpp
+++ CITY_PROPER_VERSION/0_SIMULATOR.cpp
@@ -8,12 +8,22 @@
  */
 
 #include <iostream>
+#include "CityParser.h"
+#include <sstream>
 
 int main(int argc, char* argv[]) {
 	if (argc != 2) {
 		std::cout << "Usage: " << argv[0] << " <XML-file>" << std::endl;
 		return 0;
 	}
+
+	std::stringstream ss;
+	ss << argv[0];
+	std::string filename = ss.str();
+
+	City town = CityParser::parse(filename);
+	// std::stringstream ss = Simulator::simulate(town); todo: write this function
+	// output::writeToFile(ss);	todo: write this function
 
 	return 0;
 }
