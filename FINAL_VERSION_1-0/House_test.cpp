59f4caedcc586b6600e1779f2c142e84ac2a5c16
Ruben Van Assche <rubenvanassche@gmail.com>
2013-03-19 23:28:29 +0100

    House test

--- FINAL_VERSION_1-0/House_test.cpp
+++ FINAL_VERSION_1-0/House_test.cpp
@@ -12,20 +12,80 @@
 #include "Point.h"
 #include <cstdlib>
 
-TEST(testHouse, validConstructs) {
+TEST(testHouse, constructs) {
+	const int nrTests = 10;
+	const int range = 100;
+
+	for (int i=0; i < nrTests; i++) {
+		int x0 = std::rand() % range;
+		int y0 = std::rand() % range;
+		ASSERT_NO_FATAL_FAILURE(Point Pos(x0, y0));
+		Point Pos(x0, y0);
+
+		int health = std::rand() % range + 1;
+		Size s(2, 2);
+
+		EXPECT_NO_FATAL_FAILURE(House h(Pos, s, health) );
+	}
 }
 
-TEST(testHouse, invalidConstructs) {
+TEST(testHouse, fires) {
+	const int nrTests = 10;
+	const int range = 100;
+
+	for (int i=0; i < nrTests; i++) {
+		int x0 = std::rand() % range;
+		int y0 = std::rand() % range;
+		ASSERT_NO_FATAL_FAILURE(Point Pos(x0, y0));
+		Point Pos(x0, y0);
+
+		int health = std::rand() % range + 1;
+		Size s(2, 2);
+
+		EXPECT_NO_FATAL_FAILURE(House h(Pos, s, health) );
+		House h(Pos, s, health);
+		EXPECT_TRUE(h.isBurning() == false);
+
+		EXPECT_NO_FATAL_FAILURE(h.setFire());
+		EXPECT_TRUE(h.isBurning() == true);
+		EXPECT_TRUE(h.setHealth(10));
+		EXPECT_TRUE(h.getHealth() == 10);
+		EXPECT_TRUE(h.burningDown());
+		EXPECT_TRUE(h.getHealth() == 9);
+		EXPECT_TRUE(h.stopFire());
+		EXPECT_TRUE(h.isDead() == false);
+		EXPECT_TRUE(h.burningDown());
+		EXPECT_TRUE(h.getHealth() == 9);
+		EXPECT_TRUE(h.setHealth(1));
+		EXPECT_NO_FATAL_FAILURE(h.setFire());
+		EXPECT_TRUE(h.burningDown());
+		EXPECT_TRUE(h.isDead());
+	}
 }
 
-TEST(testHouse, getters) {
-}
 
-TEST(testHouse, validSetters) {
-}
+TEST(testBuilding, copying) {
+	const int nrTests = 10;
+	const int range = 100;
 
-TEST(testHouse, invalidSetters) {
-}
+	for (int i=0; i < nrTests; i++) {
+		int x0 = std::rand() % range;
+		int y0 = std::rand() % range;
+		ASSERT_NO_FATAL_FAILURE(Point Pos(x0, y0));
+		Point Pos(x0, y0);
 
-TEST(testHouse, copying) {
+		int health = std::rand() % range + 1;
+		Size s(2, 2);
+
+		EXPECT_NO_FATAL_FAILURE(House h(Pos, s, health) );
+		House h(Pos, s, health);
+
+		EXPECT_NO_FATAL_FAILURE(House copyhouse = h);
+		House copyhouse = h;
+		EXPECT_TRUE(copyhouse.getLocation() == h.getLocation());
+		EXPECT_TRUE(copycat.getSize() == h.getSize());
+		EXPECT_TRUE(copycat.isDead() == h.isDead());
+		EXPECT_TRUE(copycat.isBurning() == h.isBurning());
+		EXPECT_TRUE(copycat.getHealth() == h.getHealth());
+	}
 }
