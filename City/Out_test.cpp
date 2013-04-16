4b7812d832fe73f054eb8df05dd0f1a88650a3ac
Ruben Van Assche <rubenvanassche@gmail.com>
2013-04-16 17:54:03 +0200

    New Out Class
    Needs to be fixed

---
+++ City/Out_test.cpp
@@ -0,0 +1,25 @@
+/*
+ * out_test.cpp
+ *
+ *  Created on: 20 mrt. 2013
+ *      Author: Ruben
+ */
+
+#include "gtest/gtest.h"
+#include "Out.h"
+#include <string>
+#include <iostream>
+#include "Point.h"
+
+TEST(testOut, file1) {
+	std::string file = "test.txt";
+	Out output(file);
+	//Point p(2,2);
+	//output << "test";
+	//output << "Test" << "\n" << p << "\n" << 21;
+	//output.indent();
+	//output << p;
+
+}
+
+
