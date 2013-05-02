ed3a5c2746b48386d359c690ba9f2162d9d09566
Ruben Van Assche <rubenvanassche@gmail.com>
2013-05-02 12:25:00 +0200

    New file test

--- City/FileTest_test.cpp
+++ City/FileTest_test.cpp
@@ -5,12 +5,12 @@
 #include "FileTest.h"
 
 TEST(FileTestTest, FileCompare) {
-	ASSERT_TRUE(FileTest::directoryExists("testOutput"));
+	ASSERT_TRUE(FileTest::directoryExists("../Output-testfiles"));
 
 	ofstream myfile;
-	myfile.open("testOutput/file1.txt");
+	myfile.open("../Output-testfiles/file1.txt");
 	myfile.close();
-	myfile.open("testOutput/file2.txt");
+	myfile.open("../Output-testfiles/file2.txt");
 	myfile.close();
 
 	//compare 2 empty files
@@ -21,46 +21,46 @@ TEST(FileTestTest, FileCompare) {
 //	EXPECT_TRUE(FileCompare("testOutput/file2.txt", "testOutput/file1.txt"));
 
 	//compare an empty and a non-empty files
-	myfile.open("testOutput/file1.txt");
+	myfile.open("../Output-testfiles/file1.txt");
 	myfile << "xxx" << endl << "yyy";
 	myfile.close();
-	EXPECT_FALSE(FileTest::compare("testOutput/file1.txt", "testOutput/file2.txt"));
-	EXPECT_FALSE(FileTest::compare("testOutput/file2.txt", "testOutput/file1.txt"));
+	EXPECT_FALSE(FileTest::compare("../Output-testfiles/file1.txt", "../Output-testfiles/file2.txt"));
+	EXPECT_FALSE(FileTest::compare("../Output-testfiles/file2.txt", "../Output-testfiles/file1.txt"));
 
 	//compare two equal files
-	myfile.open("testOutput/file2.txt");
+	myfile.open("../Output-testfiles/file2.txt");
 	myfile << "xxx" << endl << "yyy";
 	myfile.close();
-	EXPECT_TRUE(FileTest::compare("testOutput/file1.txt", "testOutput/file2.txt"));
-	EXPECT_TRUE(FileTest::compare("testOutput/file2.txt", "testOutput/file1.txt"));
+	EXPECT_TRUE(FileTest::compare("../Output-testfiles/file1.txt", "../Output-testfiles/file2.txt"));
+	EXPECT_TRUE(FileTest::compare("../Output-testfiles/file2.txt", "../Output-testfiles/file1.txt"));
 
 	//compare 2 non-empty files which are off by a character in the middle
-	myfile.open("testOutput/file2.txt");
+	myfile.open("../Output-testfiles/file2.txt");
 	myfile << "xxx" << endl << "xyy";
 	myfile.close();
-	EXPECT_FALSE(FileTest::compare("testOutput/file1.txt", "testOutput/file2.txt"));
-	EXPECT_FALSE(FileTest::compare("testOutput/file2.txt", "testOutput/file1.txt"));
+	EXPECT_FALSE(FileTest::compare("../Output-testfiles/file1.txt", "../Output-testfiles/file2.txt"));
+	EXPECT_FALSE(FileTest::compare("../Output-testfiles/file2.txt", "../Output-testfiles/file1.txt"));
 
 	//compare 2 non-empty files where one is one character shorter than the other
-	myfile.open("testOutput/file2.txt");
+	myfile.open("../Output-testfiles/file2.txt");
 	myfile << "xxx" << endl << "yy";
 	myfile.close();
-	EXPECT_FALSE(FileTest::compare("testOutput/file1.txt", "testOutput/file2.txt"));
-	EXPECT_FALSE(FileTest::compare("testOutput/file2.txt", "testOutput/file1.txt"));
+	EXPECT_FALSE(FileTest::compare("../Output-testfiles/file1.txt", "../Output-testfiles/file2.txt"));
+	EXPECT_FALSE(FileTest::compare("../Output-testfiles/file2.txt", "../Output-testfiles/file1.txt"));
 
 	//compare existig against non existing file
-	EXPECT_FALSE(FileTest::compare("testOutput/file1.txt", "testOutput/nonexisting.txt"));
-	EXPECT_FALSE(FileTest::compare("testOutput/nonexisting.txt", "testOutput/file1.txt"));
+	EXPECT_FALSE(FileTest::compare("../Output-testfiles/file1.txt", "../Output-testfiles/nonexisting.txt"));
+	EXPECT_FALSE(FileTest::compare("../Output-testfiles/nonexisting.txt", "../Output-testfiles/file1.txt"));
 }
 
 TEST(FileTestTest, fileExists){
 	ofstream myfile;
-	myfile.open("testOutput/file1.txt");
+	myfile.open("../Output-testfiles/file1.txt");
 	myfile.close();
-	myfile.open("testOutput/file2.txt");
+	myfile.open("../Output-testfiles/file2.txt");
 	myfile.close();
 
-	EXPECT_TRUE(FileTest::fileExists("testOutput/file1.txt"));
-	EXPECT_TRUE(FileTest::fileExists("testOutput/file2.txt"));
-	EXPECT_FALSE(FileTest::fileExists("testOutput/file3.txt"));
+	EXPECT_TRUE(FileTest::fileExists("../Output-testfiles/file1.txt"));
+	EXPECT_TRUE(FileTest::fileExists("../Output-testfiles/file2.txt"));
+	EXPECT_FALSE(FileTest::fileExists("../Output-testfiles/file3.txt"));
 }
